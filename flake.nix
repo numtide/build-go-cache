fba930f6a4569d49e3dc9996c3c01e03f8eb1762
zimbatm <zimbatm@zimbatm.com>
2024-07-19 12:50:06 +0200

    flake: comment broken example

--- flake.nix
+++ flake.nix
@@ -22,9 +22,10 @@
         {
           buildGoCache = pkgs.callPackage ./buildGoCache.nix { };
           get-external-imports = pkgs.callPackage ./get-external-imports.nix { };
-          example = pkgs.callPackage ./examples/buildGoModule.nix {
-            inherit (self.legacyPackages.${system}) buildGoCache;
-          };
+          # FIXME: error: cannot coerce null to a string
+          # example = pkgs.callPackage ./examples/buildGoModule.nix {
+          #   inherit (self.legacyPackages.${system}) buildGoCache;
+          # };
           example-no-cache = pkgs.callPackage ./examples/buildGoModule.nix {
             inherit (self.legacyPackages.${system}) buildGoCache;
             useGoCache = false;
